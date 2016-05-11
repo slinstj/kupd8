284b62b44f077b3546eff256d6b9bc5c44ca7bd2
Sidney Lins (slinstj) <slinstj@gmail.com>
2016-05-10 22:05:09 -0300

    zayny: a very very basic DI container

--- rawphp/useZayny/index.php
+++ rawphp/useZayny/index.php
@@ -13,7 +13,28 @@ $autoloader->addNamespace('zayny', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'zay
 
 // withou DI container
 
-$colorizer = new zayny\other\Colorizer();
-$item = new zayny\other\Movie($colorizer);
-$tv = new \zayny\other\Tv($item);
-$tv->showOnScreen();
+//$colorizer = new zayny\other\Colorizer();
+//$item = new zayny\other\Movie($colorizer);
+//$tv = new \zayny\other\Tv($item);
+//$tv->showOnScreen();
+
+// with DI container
+$container = new zayny\di\Container;
+
+$container->set('tv', function($container) {
+    $item = $container->get('watchable');
+    return new \zayny\other\Tv($item);
+});
+
+$container->set('watchable', function($container) {
+    $colorizer = $container->get('colorizable');
+    return new \zayny\other\Movie($colorizer);
+});
+
+$container->set('colorizable', function() {
+    return new \zayny\other\Colorizer();
+});
+
+
+$instance = $container->get('tv');
+$instance->showOnScreen();
--- rawphp/zayny/di/Container.php
+++ rawphp/zayny/di/Container.php
@@ -9,5 +9,16 @@ namespace zayny\di;
  */
 class Container
 {
-    
+    protected $_registers;
+
+    public function set($className, callable $callable)
+    {
+        $this->_registers[$className] = $callable;
+    }
+
+    public function get($className)
+    {
+        return call_user_func($this->_registers[$className], $this);
+    }
 }
+
